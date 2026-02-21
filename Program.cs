b6aa21a6e1c3a212c45497aba4d82f64379697cc
colewortman <wortcole12@gmail.com>
2026-02-21 14:20:04 -0600

    Learning classes, loops, and lists

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
-﻿namespace HelloWord
+﻿namespace Classes
 {
+    // class
+    class Person
+    {
+        // Fields
+        public int Id;
+        public string Name;
+        public List<int> Numbers;
+
+        // Constructor
+        public Person(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+            Numbers = new List<int>();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var person = new Person(1, "John");
+            person.Numbers.Add(10);
+            person.Numbers.Add(20);
+
+            List<string> fruits = new List<string> { "Apple", "Banana", "Cherry" };
+
+            for (int i = 0; i < fruits.Count; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+
+            Console.WriteLine(person.Id);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(string.Join(", ", person.Numbers));
         }
+
     }
 }
